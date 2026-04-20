437b2ceb7f81762e0ddb0885b029a67f87903d0a
Ahmed Noser <ahmednoseer2004@gmail.com>
2026-04-20 14:22:19 +0200

    Add new controller

---
+++ Tamkeen.API/Controllers/TicketApplicationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Tamkeen.Application.Interfaces;
+
+namespace Tamkeen.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketApplicationController(ITicketApplicationService _service) : ControllerBase
+    {
+        private string GetUserId() => User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+
+        // GET /api/TicketApplications/{ticketId}
+        [HttpGet("{ticketId}")]
+        [Authorize(Roles = "Tenant")]
+        public async Task<IActionResult> GetApplications(Guid ticketId)
+        {
+            var result = await _service.GetApplicationsAsync(ticketId, GetUserId());
+            return Ok(result);
+        }
+    }
+}
